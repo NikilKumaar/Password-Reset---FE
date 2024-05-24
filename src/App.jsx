39fa4bdf8f9fdfaa46285ff0af1f9f0f1a55b954
NikilKumaar <nikilnike666@gmail.com>
2024-05-24 16:41:07 +0530

    Password Reset - FrontEnd

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import Signup from './components/Signup'
+import Signin from './components/Signin'
+import Forgotpassword from './components/Forgotpassword'
+import { Route, Routes} from 'react-router-dom'
+import Resetpassword from './components/Resetpassword'
+function App() {
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Signin/>}></Route>
+        <Route path="/signup" element={<Signup/>}></Route>
+        <Route path="/forgotpassword" element={<Forgotpassword/>}></Route>
+        <Route path="/reset-password/:token" element={<Resetpassword/>}></Route>
+      </Routes>
+    </>
+  )
+}
+
+export default App
